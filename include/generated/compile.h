1a77c360a50f10be45f6a96b561348ae1329bff6
emwno <pulse.aashir@gmail.com>
2013-03-15 13:57:26 +0500

    N7100: Bulk update

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 15 12:38:55 PKT 2013"
+#define LINUX_COMPILE_BY "aashir"
+#define LINUX_COMPILE_HOST "satellite"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
